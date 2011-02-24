4c512661860a781f07ee9f72fd8f29ae9a10d98c
V Sreekanth <sreeix@gmail.com>
2011-02-24 22:51:37 +0530

    Removing bundler requires.

--- lib/couchup.rb
+++ lib/couchup.rb
@@ -1,3 +1,5 @@
+# require 'yajl-ruby'
+require 'couchrest'
 require  File.expand_path '../couchup/couchup', __FILE__
 Dir[File.expand_path('../couchup/commands/*.rb',__FILE__)].each { |file| require file}
 Dir[File.expand_path('../couchup/extensions/*.rb',__FILE__)].each { |file| require file}
