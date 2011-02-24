d74a2d44867d219e47982515a27eb4106fe7c15e
V Sreekanth <sreeix@gmail.com>
2011-02-24 23:09:09 +0530

    Fixing the wierd underscore problem

--- lib/couchup/extensions/string.rb
+++ lib/couchup/extensions/string.rb
@@ -2,4 +2,23 @@ class String
   def blank?
     nil || empty?
   end
+  
+  def constantize
+    Object.const_get(self)
+  end
+  
+  def camelize
+    gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+  end
+  # cargo culted from rails inflector.
+  def underscore
+    word = dup
+    word.gsub!(/::/, '/')
+    word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+    word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+    word.tr!("-", "_")
+    word.downcase!
+    word
+  end
+  
 end
--- lib/couchup/extensions/symbol.rb
+++ lib/couchup/extensions/symbol.rb
@@ -1,19 +1,13 @@
 class Symbol
   def constantize
-    Object.const_get(to_s)
-  end
-  
-  def camelize
-    to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
-  end
-  # cargo culted from rails inflector.
-  def underscore
-    word = to_s.dup
-    word.gsub!(/::/, '/')
-    word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-    word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
-    word.tr!("-", "_")
-    word.downcase!
-    word
-  end
+    to_s.constantize
+   end
+
+   def camelize
+     to_s.camelize
+   end
+   # cargo culted from rails inflector.
+   def underscore
+     to_s.underscore
+   end
 end
